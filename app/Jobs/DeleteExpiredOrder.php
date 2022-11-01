3074a130603291e1449c13b9b677ee3d0ef76a5d
Diaa Abdallah <diaaemad1@gmail.com>
2022-11-01 20:15:07 +0200

    Add Cron Job to delete Expired Order

---
+++ app/Jobs/DeleteExpiredOrder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteExpiredOrder implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Order::whereDate('created_at', '<', now()->subDays(7))
+        ->where('status', 'pending')
+        ->delete();
+    }
+}
